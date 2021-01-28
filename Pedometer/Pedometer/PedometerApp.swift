bd03a0f7340a4c07d55e5109e103b6d551842824
Matthew Newill <matt@gvenue.com>
2021-01-28 23:43:29 +0000

    first commit

---
+++ Pedometer/Pedometer/PedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerApp.swift
+//  Pedometer
+//
+//  Created by Matthew Newill on 28/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
